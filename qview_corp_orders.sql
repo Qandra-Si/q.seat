d7dcb09e466ec65e774b5735d2d036e94b912b74
Qandra Si <qandra.si@gmail.com>
2021-02-23 01:39:32 +0300

    добавлена страница корпоративные ордера

---
+++ qview_corp_orders.sql
@@ -0,0 +1,39 @@
+CREATE OR REPLACE ALGORITHM = UNDEFINED VIEW qview_corp_orders AS
+  SELECT
+    o.buy,
+    o.ticker,
+    o.wallet_division,
+    o.type_name,
+    o.volume_remain,
+    o.volume_total,
+    o.price,
+    -- o.system_id,
+    (SELECT name FROM solar_systems WHERE system_id=o.system_id) AS system_name
+  FROM
+    ( SELECT
+        -- co.is_buy_order,
+        IFNULL(co.is_buy_order,0) AS buy,
+        (SELECT ticker FROM corporation_infos WHERE corporation_id=co.corporation_id) AS ticker,
+        co.wallet_division,
+        t.typeName AS type_name,
+        co.volume_remain,
+        co.volume_total,
+        co.price,
+        -- co.location_id,
+        IF(co.location_id<100000000,
+           (SELECT system_id FROM universe_stations WHERE station_id = co.location_id),
+           (SELECT solar_system_id FROM universe_structures us WHERE structure_id = co.location_id)) AS system_id
+        -- co.updated_at
+        -- , co.*
+      FROM
+        (SELECT corporation_id, MAX(updated_at) AS time FROM corporation_orders GROUP BY 1) AS active,
+        corporation_orders co,
+        invTypes t
+      WHERE
+        co.corporation_id in (98553333,98615601,98650099) AND -- 'RI4', 'R ST', 'RI 5'
+        active.corporation_id = co.corporation_id AND
+        t.typeID = co.type_id AND
+        co.updated_at >= active.time
+    ) o
+  -- ORDER BY co.issued DESC
+  ;
---
+++ qview_industry_stat_efficiency.sql
@@ -0,0 +1,46 @@
+SELECT
+  jobs.end_date,
+  jobs.installer_id,
+  ci.name,
+  jobs.activity_id,
+  -- TIMEDIFF(jobs.end_date, jobs.start_date) AS diff,
+  jobs.duration,
+  -- SEC_TO_TIME(SUM(jobs.duration)) AS time,
+  pl.main_pilot_id,
+  IF(jobs.activity_id=1,pl.industry_lines,IF(jobs.activity_id=9,pl.reaction_lines,pl.science_lines)) AS active_lines
+FROM
+  character_infos ci,
+  ( SELECT
+      DATE(cij.end_date) AS end_date,
+      cij.installer_id,
+      cij.activity_id,
+      SUM(cij.duration) AS duration
+    FROM corporation_industry_jobs cij
+    WHERE cij.corporation_id = 98615601
+    GROUP BY 1, 2, 3
+  ) jobs,
+  ( SELECT
+      ps.pilot_id,
+      ps.main_pilot_id,
+      1 + IFNULL(ps.mp,0) + IFNULL(ps.amp,0) AS industry_lines,
+      IFNULL(ps.lo,0) + IFNULL(ps.alo,0) AS science_lines,
+      IFNULL(ps.mr,0) + IFNULL(ps.amr,0) AS reaction_lines
+    FROM
+      ( SELECT
+          p.pilot_id,
+          p.main_pilot_id,
+          (SELECT active_skill_level FROM character_skills WHERE p.pilot_id = character_id AND skill_id = 3387) AS mp, -- Mass Production
+          (SELECT active_skill_level FROM character_skills WHERE p.pilot_id = character_id AND skill_id = 24625) AS amp, -- Advanced Mass Production
+          (SELECT active_skill_level FROM character_skills WHERE p.pilot_id = character_id AND skill_id = 3406) AS lo, -- Laboratory Operation
+          (SELECT active_skill_level FROM character_skills WHERE p.pilot_id = character_id AND skill_id = 24624) AS alo, -- Advanced Laboratory Operation
+          (SELECT active_skill_level FROM character_skills WHERE p.pilot_id = character_id AND skill_id = 45748) AS mr, -- Mass Reactions
+          (SELECT active_skill_level FROM character_skills WHERE p.pilot_id = character_id AND skill_id = 45749) AS amr -- Advanced Mass Reactions
+        FROM qview_main_and_twin_ids p
+      ) ps -- pilots with skills
+  ) pl -- pilots with lines
+WHERE
+  -- jobs.activity_id = 1 AND
+  pl.pilot_id = jobs.installer_id AND
+  ci.character_id = jobs.installer_id
+ORDER BY 1 DESC
+  
