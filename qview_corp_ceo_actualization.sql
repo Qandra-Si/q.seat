4da8fca1bde2db6552590f26f8e1892f1cce60ce
Qandra Si <qandra.si@gmail.com>
2021-02-24 03:09:56 +0300

    на страницу ордеров добавил предупреждения об устаревании информации о СЕО корпораций, с тем чтобы можно было отслеживать актуальность данных на странице

---
+++ qview_corp_ceo_actualization.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE ALGORITHM = UNDEFINED VIEW qview_corp_ceo_actualization AS
+  SELECT
+    -- ci.corporation_id,
+    ci.name AS corporation_name,
+    ci.ticker,
+    -- ci.ceo_id,
+    pi.name AS ceo_name,
+    pi.updated_at AS ceo_updated,
+    IF(pi.name IS NULL OR pi.updated_at IS NULL,
+       'CEO not connected to the system !!!',
+       IF(DATEDIFF(CURDATE(),pi.updated_at)<=1,
+          NULL, -- CONCAT(pi.name,' data is up-to-date (ok).'),
+          CONCAT(pi.name,' data is outdated, orders may be out of date !!!')
+       )
+      ) AS is_actualized
+  FROM corporation_infos ci
+         LEFT OUTER JOIN character_infos pi ON (pi.character_id = ci.ceo_id)
+  WHERE ci.corporation_id IN (98615601,98553333,98650099) -- 'RI4','R ST','R I5'
+  ;
