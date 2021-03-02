7198dac5374786fba72b839bd605eece1dfded52
Qandra Si <qandra.si@gmail.com>
2021-03-03 00:40:28 +0300

    исправлена ошибка : неправильно группировались данные в planetary jobs, поэтому терялась часть данных, если пилот варил планетарку разных tier

--- qview_planetary_statistics.sql
+++ qview_planetary_statistics.sql
@@ -8,6 +8,6 @@ CREATE OR REPLACE ALGORITHM = UNDEFINED VIEW qview_planetary_statistics AS
     SUM(pj.volume) AS volume
   FROM
     qview_pilots_planetary_jobs pj
-  GROUP BY 1, 2, 3
+  GROUP BY 1, 2, 3, 4
   -- ORDER BY 1 DESC
   ;
